b567a1f39bcd8ffca3b94799f712918db2a11d3e
Alexander <alsavenkoweb@gmail.com>
2022-04-19 13:33:09 +0300

    add movie data base and change some things in prev code

--- js learning/js/script.js
+++ js learning/js/script.js
@@ -30,14 +30,21 @@
 
 // console.log(isChecked || isClose);
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', "");
-console.log(numberOfFilms);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
 const personalMovieDB = {
-    count: console.log(numberOfFilms),
-    movies: "",
-    actors: "",
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
     genres: [],
     privat: false
 };
+
+const a = prompt('Один из последних просмотреных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотреных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
