c1cb547f744872d71b390ed1e97f8c6c5efe557e
Alexander <alsavenkoweb@gmail.com>
2022-04-20 14:52:35 +0300

    learn if else, switch case

--- js learning/js/script.js
+++ js learning/js/script.js
@@ -30,22 +30,61 @@
 
 // console.log(isChecked || isClose);
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+// Первое консольное приложение 
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
-const a = prompt('Один из последних просмотреных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотреных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const a = prompt('Один из последних просмотреных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотреных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-console.log(personalMovieDB);
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// Первое консольное приложение
+
+// if (4 == 4) {   //if (1)
+//     console.log('Ok!');
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('To much');
+// } else {
+//     console.log('Ok');
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('Wrong');
+//         break;
+//     case 100:
+//         console.log('Wrong');
+//         break;
+//     case 50:
+//         console.log('Right');
+//         break;
+//     default:
+//         console.log('Not today');
+//         break;
+// }
