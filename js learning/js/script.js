706b3c5ca8318f5cff8ada41dbadc43c22cc4290
Alexander <alsavenkoweb@gmail.com>
2022-04-21 14:42:23 +0300

    learn do while for

--- js learning/js/script.js
+++ js learning/js/script.js
@@ -162,3 +162,26 @@
 
 // console.log(!0);
 
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// let num = 50;
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+    
+//     console.log(i);
+// }
