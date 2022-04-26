00f108cd53f6b8de22f5b9238477759ca9479f75
Alexander <alsavenkoweb@gmail.com>
2022-04-26 18:11:07 +0300

    update console app

--- js learning/js/script.js
+++ js learning/js/script.js
@@ -344,3 +344,40 @@
 
 // console.log(result);
 // Упражнения по написанию кода
+
+// Улучшеное консольное приложение
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотреных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         i--;
+//     }  
+// }
+
+// if (personalMovieDB.count < 10) {
+//     console.log('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('Вы классический зритель');
+// } else if (personalMovieDB.count >= 30) {
+//     console.log('Вы киноман');
+// } else {
+//     console.log('Произошла ошибка');
+// }
+
+//console.log(personalMovieDB);
+
+// Улучшеное консольное приложение
