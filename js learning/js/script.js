075bbb011b985e64e98765ce0352cae9ffe15aeb
Alexander <alsavenkoweb@gmail.com>
2022-04-29 23:09:43 +0300

    coding exercise_2

--- js learning/js/script.js
+++ js learning/js/script.js
@@ -446,3 +446,35 @@
 // }
 // test();
 
+// function sayHello(name) {
+//     return `Привет, ${name}!`;
+// }
+
+// sayHello('Антон');
+
+// function returnNeighboringNumbers(num) {
+//     return [num - 1, num, num + 1];
+// }
+
+// returnNeighboringNumbers(5);
+
+// function getMathResult(num, times) {
+//     if (typeof(times) !== 'number' || times <= 0) {
+//         return num;
+//     }
+
+//     let str = '';
+
+//     for (let i = 1; i <= times; i++) {
+//         if (i === times) {
+//             str += `${num * i}`;
+//         } else {
+//             str += `${num * i}---`;
+//         }
+//     }
+
+//     return str;
+// }
+
+// getMathResult(5, 3);
+
