e4905e6f6833d84eabb39e9238e7fbeaf7f961f7
Alexander <alsavenkoweb@gmail.com>
2022-05-02 17:41:18 +0300

    added new function into console apps

--- js learning/js/script.js
+++ js learning/js/script.js
@@ -32,7 +32,17 @@
 
 // Первое консольное приложение 
 
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+// let numberOfFilms;
+
+// function start() {
+//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+//     }
+// }
+
+// start();
 
 // const personalMovieDB = {
 //     count: numberOfFilms,
@@ -347,38 +357,70 @@
 
 // Улучшеное консольное приложение
 
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+let numberOfFilms;
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
-// for (let i = 0; i < 2; i++) {
-//     const a = prompt('Один из последних просмотреных фильмов?', ''),
-//           b = prompt('На сколько оцените его?', '');
-    
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//     } else {
-//         i--;
-//     }  
-// }
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+    }
+}
 
-// if (personalMovieDB.count < 10) {
-//     console.log('Просмотрено довольно мало фильмов');
-// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//     console.log('Вы классический зритель');
-// } else if (personalMovieDB.count >= 30) {
-//     console.log('Вы киноман');
-// } else {
-//     console.log('Произошла ошибка');
-// }
+start();
 
-//console.log(personalMovieDB);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотреных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+        
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }  
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
 
 // Улучшеное консольное приложение
 
