8aa3ae310133ab5c8ef3e05325423c9c63ab562e
Alexander <alsavenkoweb@gmail.com>
2022-05-13 13:10:04 +0300

    learn callback function

--- js learning/js/script.js
+++ js learning/js/script.js
@@ -648,3 +648,14 @@
 // }
 // fib(4);
 
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
+
+// function done() {
+//     console.log('Я прошел этот урок!');
+// }
+
+// learnJS('JavaScript', done);
+
